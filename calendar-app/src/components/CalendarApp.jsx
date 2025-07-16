49c34a0bc8683b01772c4a4b0cbb4aba34057ac4
Rahul Malhotra <rahulmalhotra236@gmail.com>
2025-07-16 13:45:26 +0530

    updated

--- calendar-app/src/components/CalendarApp.jsx
+++ calendar-app/src/components/CalendarApp.jsx
@@ -21,16 +21,30 @@ const CalendarApp = () => {
   const [currentMonth, setCurrentMonth] = useState(currentDate.getMonth())
   const [currentYear, setCurrentYear] = useState(currentDate.getFullYear())
 
+  const [selectedDate, setSelectedDate] = useState(currentDate)
+  const [showEventPopup, setShowEventPopup]= useState(false)
+
   const daysInMonth = new Date(currentYear, currentMonth+1, 0).getDate()
   const firstDayOfMonth = new Date(currentYear, currentMonth, 1).getDay()
 
   const prevMonth=()=>{
     setCurrentMonth((prevMonth) => prevMonth === 0 ? 11 : prevMonth - 1)
-    setCurrentYear((prevYear) => prevYear === 0 ? prevYear - 1:prevYear)
+    setCurrentYear((prevYear) => currentMonth === 0 ? prevYear - 1:prevYear)
   }
   const nextMonth=()=>{
     setCurrentMonth((prevMonth) => (prevMonth === 11 ? 0 : prevMonth + 1))
     setCurrentYear((prevYear) => (currentMonth === 11 ? prevYear + 1 : prevYear))
+  }
+
+  const handleDayClick = (day) => {
+
+    const clickedDate = new Date(currentYear, currentMonth, day)
+    const today = new Date()
+    if (clickedDate >= today) {
+      setSelectedDate(clickedDate)
+      setShowEventPopup(true )
+    }
+    
   }
 
   console.log(currentYear, currentMonth + 1, daysInMonth,daysOfWeek[ firstDayOfMonth])
@@ -50,49 +64,60 @@ const CalendarApp = () => {
 
         <div className="weekdays">
           {daysOfWeek.map((day) => (
-            <span key={day} >{day}</span>
+            <span key={day}>{day}</span>
           ))}
         </div>
 
         <div className="days">
-          {[
-            ...Array(firstDayOfMonth)
-              .keys()]
-              .map((_, index) => <span key={`empty-{index}`} />)
-          }
-          {[
-            ...Array(daysInMonth)
-              .keys()]
-            .map((day) => <span key={`empty-{index}`} className={day + 1 === currentDate.getDate() && currentMonth === currentDate.getMonth() && currentYear === currentDate.getFullYear() ? "current-day" : ""} >{day+1}</span>)
-          }
+          {[...Array(firstDayOfMonth).keys()].map((_, index) => (
+            <span key={`empty-${index}`} />
+          ))}
+          {[...Array(daysInMonth).keys()].map((day) => (
+            <span
+              key={day + 1}
+              className={
+                day + 1 === currentDate.getDate() &&
+                currentMonth === currentDate.getMonth() &&
+                currentYear === currentDate.getFullYear()
+                  ? "current-day"
+                  : ""
+              }
+              onClick={() => handleDayClick(day + 1)}
+            >
+              {day + 1}
+            </span>
+          ))}
         </div>
       </div>
 
       <div className="events">
-        <div className="event-popup">
-          <div className="time-input">
-            <div className="event-popup-time">Time</div>
-            <input
-              type="number"
-              name="hours"
-              min={0}
-              max={24}
-              className="hours"
-            />
-            <input
-              type="number"
-              name="minutes"
-              min={0}
-              max={60}
-              className="minutes"
-            />
+        {showEventPopup && (
+          <div className="event-popup">
+            <div className="time-input">
+              <div className="event-popup-time">Time</div>
+              <input
+                type="number"
+                name="hours"
+                min={0}
+                max={24}
+                className="hours"
+              />
+              <input
+                type="number"
+                name="minutes"
+                min={0}
+                max={60}
+                className="minutes"
+              />
+            </div>
+            <textarea placeholder="Enter event Text (Maximum 60 Characters)"></textarea>
+            <button className="event-popup-btn">Add Event</button>
+            <button className="close-event-popup ">
+              <i className="bx bx-x" onClick={() => setShowEventPopup(false)}></i>
+            </button>
           </div>
-          <textarea placeholder="Enter event Text (Maximum 60 Characters)"></textarea>
-          <button className="event-popup-btn">Add Event</button>
-          <button className="close-event-popup ">
-            <i className="bx bx-x"></i>
-          </button>
-        </div>
+        )}
+
         <div className="event">
           <div className="event-date-wrapper">
             <div className="event-date">May 15, 2024</div>
